28aa4c6a0417b4847ae29e1628e531ec092552f1
QUYNHTD-PC\MHC <quynhtd@oneempower.com>
2024-07-02 11:44:23 +0700

    add lombox

--- src/test/java/com/quynhtd/source_code_final/SourceCodeFinalApplicationTests.java
+++ src/test/java/com/quynhtd/source_code_final/SourceCodeFinalApplicationTests.java
@@ -5,10 +5,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SourceCodeFinalApplicationTests {
+public class SourceCodeFinalApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
